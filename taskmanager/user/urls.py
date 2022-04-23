574913c5ef0c76c3153f2ea616d15c89c5b05c8f
ayymeredova <73907025+ayymeredova@users.noreply.github.com>
2022-04-24 00:51:15 +0300

    Feature/6 главная страницаfront end + ссылки (#33)
    Главная страница

---
+++ taskmanager/user/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+
+app_name = 'user'
+
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    #path('login/', views.LoginClass.as_view(), name='login'),
+    #path('signup/', views.SignupView.as_view(), name='signup'),
+]
---
+++ taskmanager/user/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, 'user/index.html')
