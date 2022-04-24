c123b72b4d414e546add1d82cb9c99541a6430ca
Anothermind9912 <petr9912@bk.ru>
2022-04-24 20:53:28 +0300

    Form added

--- task/forms.py
+++ task/forms.py
@@ -10,3 +10,13 @@ class ProjectForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
         }
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ("title", "description", "deadline")
+        widgets = {
+           'deadline': forms.DateTimeInput(attrs={'class': 'datetimepicker1 form-control' }),
+           'title' : forms.TextInput(attrs={'class': 'form-control'}),
+           'description' : forms.Textarea(attrs={'class': 'form-control', 'rows':'10'}),
+        }
