b12dc57cb31c2149fc3fc6972a62076c4ada14ce
ayymeredova <73907025+ayymeredova@users.noreply.github.com>
2022-04-25 02:41:02 +0300

    Feature/9 страница об авторахfront end (#43)
    * Редактировать user/userauthors.html
    
    * Добавить страницу об авторах
    
    * Обновить миграции
    
    Co-authored-by: ayymeredova <ayjahanmeredova17@mail.com>
    Co-authored-by: PuPha <47568867+ppupha@users.noreply.github.com>

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -18,6 +18,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
--- taskmanager/user/views.py
+++ taskmanager/user/views.py
@@ -4,3 +4,7 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'user/index.html')
+
+class AuthorsView(View):
+    def get(self, request):
+        return render(request, 'user/authors.html')
