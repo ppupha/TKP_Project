e5782b7bc660adfde8b396183d02a37d0a4c0691
K-COMPUTER\ADMIN <nguyensanghso@gmail.com>
2022-04-25 02:10:01 +0300

    Add CSS

--- task/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Project, Task, Notification
-# Register your models here.
-
-admin.site.register(Project)
-admin.site.register(Task)
-admin.site.register(Notification)
--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIR = os.path.join(BASE_DIR, 'static/')
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
